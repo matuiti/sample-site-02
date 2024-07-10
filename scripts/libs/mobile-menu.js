9ccf617534afe9c4c377a9b7d4a0cde14ea1e351
matuiti <ichi.animo@gmail.com>
2024-07-10 23:14:38 +0900

    menuセクションの画像設置（差し替え前のもの）まで完了

---
+++ scripts/libs/mobile-menu.js
@@ -0,0 +1,21 @@
+class MobileMenu {
+  constructor() {
+    this.DOM = {}
+    this.DOM.btn = document.querySelector('.mobile-menu__btn')
+    this.DOM.container = document.querySelector('#global-container')
+    this.eventType = this.#getEventType();
+    this.#addEvent();
+  }
+  #getEventType() {
+    const isTouchCapable = "ontouchstart" in window;
+
+    return isTouchCapable ? "touchstart" : "click";
+  }
+  #toggle() {
+    this.DOM.container.classList.toggle("menu-open");
+  }
+
+  #addEvent() {
+    this.DOM.btn.addEventListener(this.eventType, this.#toggle.bind(this));
+  }
+}
