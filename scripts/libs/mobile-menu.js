525f5cb20ba00dad3f15588745ab0e41abfffcc2
matuiti <you@example.com>
2024-09-06 15:58:24 +0900

    linktext fix

--- scripts/libs/mobile-menu.js
+++ scripts/libs/mobile-menu.js
@@ -2,7 +2,7 @@ class MobileMenu {
   constructor() {
     this.DOM = {}
     this.DOM.btn = document.querySelector('.mobile-menu__btn')
-    this.DOM.link = document.querySelector('.mobile-menu__link')
+    this.DOM.links = document.querySelectorAll('.mobile-menu__link')
     this.DOM.container = document.querySelector('#global-container')
     this.eventType = this.#getEventType();
     this.#addEvent();
@@ -18,6 +18,8 @@ class MobileMenu {
 
   #addEvent() {
     this.DOM.btn.addEventListener(this.eventType, this.#toggle.bind(this));
-    this.DOM.link.addEventListener(this.eventType, this.#toggle.bind(this));
+    this.DOM.links.forEach(link => {
+      link.addEventListener(this.eventType, this.#toggle.bind(this));
+    });
   }
 }
