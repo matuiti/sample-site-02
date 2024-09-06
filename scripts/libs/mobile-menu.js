beb8541d4eefacf583e4250784707f45a59030d8
matuiti <you@example.com>
2024-09-06 15:51:15 +0900

    mobile-menuをtextlinkをクリックしても閉じるように変更、textlinkにセクション追加

--- scripts/libs/mobile-menu.js
+++ scripts/libs/mobile-menu.js
@@ -2,6 +2,7 @@ class MobileMenu {
   constructor() {
     this.DOM = {}
     this.DOM.btn = document.querySelector('.mobile-menu__btn')
+    this.DOM.link = document.querySelector('.mobile-menu__link')
     this.DOM.container = document.querySelector('#global-container')
     this.eventType = this.#getEventType();
     this.#addEvent();
@@ -17,5 +18,6 @@ class MobileMenu {
 
   #addEvent() {
     this.DOM.btn.addEventListener(this.eventType, this.#toggle.bind(this));
+    this.DOM.link.addEventListener(this.eventType, this.#toggle.bind(this));
   }
 }
