46b6216489864ba484d66abc04654667c8c8f150
matuiti <ichi.animo@gmail.com>
2024-09-15 14:47:58 +0900

    add to fade in animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
 class Main {
-    #observers = [];
+    #observers = []
     constructor() {
+        this.header = document.querySelector('.header')
         this.#init()
     }
     #init() {
         new Hero
         new MobileMenu
+        this.#scrollInit()
+    }
+    #scrollInit() {
+        this.#observers.push(
+            new ScrollObserver('.appear', this.#inviewAnimation)
+        )
+        console.log(this.#observers);
+    }
+
+    #inviewAnimation(el, inview) {
+        if(inview) {
+            el.classList.add('inview')
+        } else {
+            el.classList.remove('inview')
+        }
     }
 }
-new Main
+const main = new Main
